0ec57cd332b3b2645daf9d9ae133d29cc3570896
Nataya Soledad <natayafs@gmail.com>
2020-10-29 01:45:11 -0300

    Add logo

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text, View} from '../components/Themed';
+import { Image } from 'react-native';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
+      <Image source={require('./../assets/images/mit.png')} style={{ width: 100, height: 120 }} />
       <Text style={styles.title}>MIT</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
